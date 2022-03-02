0ebe99c5a008290bbb45f891b879eb9c8cb8224e
coffee <1207294723@qq.com>
2022-03-02 18:59:00 +0800

    添加秒杀倒计时 在当前秒杀范围内显示秒杀按钮，未到时间显示倒计时，过时秒杀按钮变灰

--- src/main/java/com/coffee/controller/GoodsController.java
+++ src/main/java/com/coffee/controller/GoodsController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -31,8 +33,33 @@ public class GoodsController {
     }
 
     @RequestMapping("/toDetail/{goodsId}")
-    public String toDetail(Model model,@PathVariable Long goodsId){
+    public String toDetail(Model model,@RequestAttribute(name="user") User user,@PathVariable Long goodsId){
+        model.addAttribute("user",user);
         GoodsVo goodsVo=goodsService.findGoodsVoById(goodsId);
+
+        Date startTime=goodsVo.getStartTime();
+        Date endTime=goodsVo.getEndTime();
+        Date nowDate = new Date();
+        //秒杀状态
+        int seckillStatus=0;
+        //秒杀倒计时
+        int remainSeconds=0;
+        //秒杀还未开始
+        if (nowDate.before(startTime)){
+            remainSeconds=(int)((startTime.getTime()-nowDate.getTime())/1000);
+        }
+        //秒杀已经结束
+        else if (nowDate.after(endTime)){
+            seckillStatus=2;
+            remainSeconds=-1;
+        }
+        //秒杀中
+        else{
+            seckillStatus=1;
+            remainSeconds=0;
+        }
+        model.addAttribute("seckillStatus",seckillStatus);
+        model.addAttribute("remainSeconds",remainSeconds);
         model.addAttribute("goods",goodsVo);
 
         return "goodsDetail";
